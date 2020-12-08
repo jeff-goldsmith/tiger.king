3931eb776e28cc9c3a4be506ab0fce7513e64628
Jeff Goldsmith <jeff.goldsmith@columbia.edu>
2020-12-08 10:36:17 -0500

    Initial commit

---
+++ R/tiger_king.R
@@ -0,0 +1,15 @@
+#' Tiger King
+#'
+#' Print tiger king using emojis
+#'
+#' @return A printed emoji
+#' @export
+#'
+#' @examples
+#'
+#' tiger_king()
+#'
+tiger_king = function() {
+  emo::ji("tiger")
+  emo::ji("king")
+}
